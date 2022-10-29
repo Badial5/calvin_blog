a5c77f1088ecdb21c731752c9268060e0530697d
Badial5 <omarbadialstudent@gmail.com>
2022-10-29 12:15:20 +0000

    Initial Project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Awesome Blog</h1>
+      <div className='page-body'>
+          Welcome to my blog
+      </div>
+    </div>
+  )
+}
+
+export default App
